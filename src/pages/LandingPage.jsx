e92770b679f0a4bd513cfa9b4456dae59574779e
hadinuryani <wongsedah6@gmail.com>
2026-06-06 16:35:20 +0700

    update cursor

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -120,15 +120,15 @@ export default function LandingPage() {
   return (
     <div className="landing-page">
       <SplashCursor
-        SIM_RESOLUTION={128}
-        DYE_RESOLUTION={1440}
-        DENSITY_DISSIPATION={3.5}
-        VELOCITY_DISSIPATION={0.5}
-        PRESSURE={0.1}
-        CURL={3}
-        SPLAT_RADIUS={0.2}
-        SPLAT_FORCE={6000}
-        COLOR_UPDATE_SPEED={10}
+         SIM_RESOLUTION={256}
+    DYE_RESOLUTION={1664}
+    DENSITY_DISSIPATION={3.5}
+    VELOCITY_DISSIPATION={1}
+    PRESSURE={0.1}
+    CURL={0}
+    SPLAT_RADIUS={0.2}
+    SPLAT_FORCE={3500}
+    COLOR_UPDATE_SPEED={40}
       />
       <Navbar variant="public" />
 
